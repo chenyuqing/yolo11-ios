b845f85026d0ce2204da280de410c9559aa9faa7
Tsing Chan <motoleisure@163.com>
2025-08-03 21:24:36 +0800

    Initial Commit

---
+++ yolo11/yolo11App.swift
@@ -0,0 +1,17 @@
+//
+//  yolo11App.swift
+//  yolo11
+//
+//  Created by tim on 2025/8/3.
+//
+
+import SwiftUI
+
+@main
+struct yolo11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yolo11Tests/yolo11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  yolo11Tests.swift
+//  yolo11Tests
+//
+//  Created by tim on 2025/8/3.
+//
+
+import Testing
+@testable import yolo11
+
+struct yolo11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
